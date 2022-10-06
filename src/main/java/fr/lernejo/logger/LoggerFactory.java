3c6cbcc15468bd1dd08459738f3062f75373b76a
VineBast <vine.bast@gmail.com>
2022-10-06 10:51:29 +0200

    Setup Logger Package

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,11 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory implements Logger {
+    @Override
+    public void log(String message) {}
+
+    public static Logger getLogger(String name) {
+
+        return null;
+    }
+}
