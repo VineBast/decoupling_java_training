062368c4666872b3ddacf64145a15e79f4bba439
VineBast <vine.bast@gmail.com>
2022-10-10 15:19:04 +0200

    Setup FilteredLogger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,8 +1,11 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory  {
     public static Logger getLogger(String name) {
+        Predicate<String> condition = message -> message.contains("simulation");
         return new CompositeLogger( new ConsoleLogger(),
-            new ContextualLogger(name, new FileLogger("log.txt")));
+            new ContextualLogger(name, new FilteredLogger(new FileLogger("log.txt"), condition)));
     }
 }
